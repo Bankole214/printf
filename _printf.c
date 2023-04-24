3156cc2fcec7c212d3cc8514574e44472e9fb43c
Bankole214 <adegunlenurudeen214@gmail.com>
2023-04-24 15:00:43 -0700

    update

--- _printf.c
+++ _printf.c
@@ -1,6 +1,8 @@
 #include "main.h"
+#include <stdlib.h>
 
-void print_buffer(char buffer[], int *buff_ind);
+
+void print_con(char con[], int *buff_ind);
 
 /**
  * _printf - Printf function
@@ -9,35 +11,35 @@ void print_buffer(char buffer[], int *buff_ind);
  */
 int _printf(const char *format, ...)
 {
-	int i, printed = 0, printed_chars = 0;
+	int a, printed = 0, printed_chars = 0;
 	int flags, width, precision, size, buff_ind = 0;
 	va_list list;
-	char buffer[BUFF_SIZE];
+	char con[BUFF_SIZE];
 
 	if (format == NULL)
 		return (-1);
 
 	va_start(list, format);
 
-	for (i = 0; format && format[i] != '\0'; i++)
+	for (a = 0; format && format[i] != '\0'; a++)
 	{
 		if (format[i] != '%')
 		{
-			buffer[buff_ind++] = format[i];
+			con[buff_ind++] = format[a];
 			if (buff_ind == BUFF_SIZE)
-				print_buffer(buffer, &buff_ind);
-			/* write(1, &format[i], 1);*/
+				print_con(con, &buff_ind);
+			/* write(1, &format[a], 1);*/
 			printed_chars++;
 		}
 		else
 		{
-			print_buffer(buffer, &buff_ind);
-			flags = get_flags(format, &i);
-			width = get_width(format, &i, list);
-			precision = get_precision(format, &i, list);
-			size = get_size(format, &i);
-			++i;
-			printed = handle_print(format, &i, list, buffer,
+			print_con(con, &buff_ind);
+			flags = get_flags(format, &a);
+			width = get_width(format, &a, list);
+			precision = get_precision(format, &a, list);
+			size = get_size(format, &a);
+			++a;
+			printed = handle_print(format, &a, list, con,
 				flags, width, precision, size);
 			if (printed == -1)
 				return (-1);
@@ -45,7 +47,7 @@ int _printf(const char *format, ...)
 		}
 	}
 
-	print_buffer(buffer, &buff_ind);
+	print_con(con, &buff_ind);
 
 	va_end(list);
 
@@ -53,14 +55,14 @@ int _printf(const char *format, ...)
 }
 
 /**
- * print_buffer - Prints the contents of the buffer if it exist
- * @buffer: Array of chars
+ * print_con - Prints the contents of the buffer if it exist
+ * @con: Array of chars
  * @buff_ind: Index at which to add next char, represents the length.
  */
-void print_buffer(char buffer[], int *buff_ind)
+void print_con(char con[], int *buff_ind)
 {
 	if (*buff_ind > 0)
-		write(1, &buffer[0], *buff_ind);
+		write(1, &con[0], *buff_ind);
 
 	*buff_ind = 0;
 }
